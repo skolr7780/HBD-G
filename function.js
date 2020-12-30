f909c2c35c3bb0332b968f6135377b314442970d
skolr7780 <45235249+skolr7780@users.noreply.github.com>
2020-12-30 10:32:17 -0800

    Delete function.js

--- function.js
+++
@@ -1,5 +0,0 @@
-alert("،كل عآم وضحكاتك انغام تسعدني،كل عآم وانتي ععُمري الحلو ونعيمي الدنيوي❤🎄.");
-window.addEventListener('click', function() {
-    var audio = document.getElementById("HBD");
-    audio.play();
-});
